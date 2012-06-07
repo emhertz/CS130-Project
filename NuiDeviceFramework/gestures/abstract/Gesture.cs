e5ad28c2032ad964bea00584d53d1ec44e40d854
pbotros1234 <pbotros1234@gmail.com>
2012-06-07 02:47:48 -0700

    Bow gesture

--- NuiDeviceFramework/gestures/abstract/Gesture.cs
+++ NuiDeviceFramework/gestures/abstract/Gesture.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using NuiDeviceFramework.datatypes.skeleton.enums;
 using NuiDeviceFramework.devices;
-using NuiDeviceFramework.reflection;
 
 namespace NuiDeviceFramework.Gestures
 {
